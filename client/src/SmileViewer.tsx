89752de0878519168d0873c2c18ad35bb9dea891
Michael Flaherty <Michael.Flaherty@modernatx.com>
2025-03-30 19:26:49 -0400

    Display image on left side of component, coordinates on right

--- client/src/SmileViewer.tsx
+++ client/src/SmileViewer.tsx
@@ -11,49 +11,53 @@ const SmileViewer: React.FC<Props> = ({ image, coords }) => {
   if (!image || !coords || coords.length === 0) return null;
 
   return (
-    <div className="card" style={{ maxWidth: "350px", margin: "0 auto" }}>
-      <div className="card-header text-center">
-        Found Smile
-      </div>
+    <div className="card" style={{ maxWidth: "750px", margin: "0 auto" }}>
+      <div className="card-header text-center">Found Smile</div>
       <div className="card-body">
-        <img
-          src={image}
-          alt="Detected Smile"
-          className="img-fluid mb-3"
-          style={{ display: "block", margin: "0 auto" }}
-        />
-        <h5 className="card-title text-center">Smile Coordinates</h5>
-        <div className="table-responsive">
-          <table className="table table-bordered">
-            <thead>
-              <tr className="text-center">
-                <th>Property</th>
-                <th>Value</th>
-              </tr>
-            </thead>
-            <tbody>
-              {coords.map((coord, index) => (
-                <React.Fragment key={index}>
+        <div className="row">
+          <div className="col-md-6 d-flex align-items-center justify-content-center">
+            <img
+              src={image}
+              alt="Detected Smile"
+              className="img-fluid"
+              style={{ maxWidth: "100%" }}
+            />
+          </div>
+          <div className="col-md-6">
+            <h5 className="text-center">Smile Coordinates</h5>
+            <div className="table-responsive">
+              <table className="table table-bordered">
+                <thead>
                   <tr className="text-center">
-                    <td>h</td>
-                    <td>{coord.h}</td>
+                    <th>Property</th>
+                    <th>Value</th>
                   </tr>
-                  <tr className="text-center">
-                    <td>w</td>
-                    <td>{coord.w}</td>
-                  </tr>
-                  <tr className="text-center">
-                    <td>x</td>
-                    <td>{coord.x}</td>
-                  </tr>
-                  <tr className="text-center">
-                    <td>y</td>
-                    <td>{coord.y}</td>
-                  </tr>
-                </React.Fragment>
-              ))}
-            </tbody>
-          </table>
+                </thead>
+                <tbody>
+                  {coords.map((coord, index) => (
+                    <React.Fragment key={index}>
+                      <tr className="text-center">
+                        <td>h</td>
+                        <td>{coord.h}</td>
+                      </tr>
+                      <tr className="text-center">
+                        <td>w</td>
+                        <td>{coord.w}</td>
+                      </tr>
+                      <tr className="text-center">
+                        <td>x</td>
+                        <td>{coord.x}</td>
+                      </tr>
+                      <tr className="text-center">
+                        <td>y</td>
+                        <td>{coord.y}</td>
+                      </tr>
+                    </React.Fragment>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
         </div>
       </div>
     </div>
